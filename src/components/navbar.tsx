ded7029e8a49060a9eafb59020313e1292c69e86
Tim Reber <metimreber@gmail.com>
2024-11-12 14:41:16 +0100

    fix: eslint errors

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,11 +7,9 @@ import { cn } from "@/lib/utils";
 
 import {
   NavigationMenu,
-  NavigationMenuContent,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-  NavigationMenuTrigger,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { Button } from "./ui/button";
