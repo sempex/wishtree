054e909e9a044f3d440b850826747c1a444663f6
Tim Reber <metimreber@gmail.com>
2024-11-29 15:44:26 +0100

    feat: add copy functionallity

--- src/app/dashboard/family/[slug]/page.tsx
+++ src/app/dashboard/family/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { getFamily } from "@/components/dashboard/family/actions";
 import AddMember from "@/components/dashboard/family/add-member";
 import MemberCard from "@/components/dashboard/family/member-card";
-import { Button } from "@/components/ui/button";
+import { ShareLink } from "@/components/dashboard/family/share-link";
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
@@ -13,7 +13,7 @@ export default async function Page({ params }: { params: Promise<{ slug: string 
         <p className="font-bold text-2xl">{family?.name.toLocaleUpperCase()}</p>
         <div className="flex gap-2">
           <AddMember slug={slug}/>
-          <Button>Share Link</Button>
+          <ShareLink baseUrl={process.env.APP_URL ?? "example.com"} slug={slug} />
         </div>
       </div>
       <div className="flex justify-end mt-4">
